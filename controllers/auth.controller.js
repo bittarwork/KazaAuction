62d614cfe908406a9a03140325e9fe07a17e7987
bittarov <81069087+bittarwork@users.noreply.github.com>
2024-04-28 14:10:12 +0300

    init server and git repo

---
+++ controllers/auth.controller.js
@@ -0,0 +1,85 @@
+const Schema = require('../models/user.schema');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+// user authentication
+exports.signIn = (req, res) => {
+    // validate request
+    let {email,password} = req.body;
+    console.log(req.body);
+    if (!email) return res.status(400).send({ message: 'Email must be filled !' });
+    if (!password) return res.status(400).send({ message: 'Password must be filled !' });
+    if (password.length < 8) return res.status(400).send({ message: 'Password must be equal or more than 8 character !' });
+    // check email already exist or not
+    try {
+        Schema.findOne({ email: email }).then((user)=>{
+            if (!user) {
+                console.log('Email not found in database.');
+                return res.status(404).send({ message: 'Email not found, please register!.'}); 
+            }
+            else if (user) {
+                console.log('Email found in database.');
+                // comparing passwords
+                var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+                if (!passwordIsValid) {
+                    console.log('Email registered, but password is wrong.');
+                    return res.status(401).send({
+                        message: 'Email registered, but password is wrong.',
+                        token: null,
+                    });
+                }
+                else if (passwordIsValid) {
+                    // sign in token create from user id
+                    var accessToken = jwt.sign(
+                        {id: user._id},
+                        process.env.JWT_SECRET,
+                        {expiresIn: 86400},
+                    );
+                    console.log('Authenticated!');
+                    console.log('Token: ', accessToken);
+                    return res.status(200).send({
+                        user: {
+                            id: user._id,
+                            name: user.name,
+                            email: user.email,
+                            role: user.role,
+                        },
+                        token: accessToken,
+                    });
+                }
+            }
+        })
+    }
+    catch(err) { return res.status(500).send({ message: err || 'An error occured.'}); }
+}
+
+// user authorization with verify the access token
+exports.verifyAccessToken = (req, res) => {
+    const header = req.headers.authorization;
+    const authHeader = req.headers['authorization']; // header and authHeader are same
+    const token = authHeader.split(' ')[1];
+    console.log(authHeader);
+    console.log(header);
+    console.log(token);
+    // validation
+    if (!authHeader) { return res.status(403).send({message: 'request header undefined'}); }
+    // convert token to json (decoded)
+    const decodedResult = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decodedResult.id);
+    Schema.findOne({ _id: decodedResult.id }).then(user => {
+        if (user.role === 'admin') {
+            console.log(user.name);
+            return res.status(200).send({
+                message: 'congratulations! there is no hidden content',
+                name: user.name,
+            });
+        }
+        console.log(user.name);
+        return res.status(200).send({
+            message: 'congratulations! but there is a hidden content',
+            name: user.name,
+        });
+    }).catch(err => { 
+        return res.status(401).send({message: 'invalid jwt token'}); 
+    });
+};
---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const createError = require('http-errors');
+
+// enable env variable
+require('dotenv').config();
+
+// cors
+const cors = require('cors');
+app.use(cors());
+
+// loging server side request
+app.use((req,res,next)=>{
+  let reqBody = req.body;
+  let reqMethode = req.method;
+  let reqUrl = req.url;
+  console.log(`a request has arrived to the server with this information:
+    \t request body: ${reqBody}
+    \t request method: ${reqMethode}
+    \t request URL: ${reqUrl}
+    `)
+    next();
+})
+// middleware 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// mongodb connection
+require('./config/mongodb.config').sync;
+
+// routes
+const routes = require('./routes/routes.js');
+app.get('/',(req,res) => { 
+  return res.status(200).send({ message:'Welcome to express-mongodb app' }); 
+});
+app.use('/api', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// server 
+const port = process.env.PORT || 5000;
+app.listen(port, function(err) {
+  if (err) console.log(err);
+  console.log("Server running on ", port," in ",app.settings.env," mode.");
+});
